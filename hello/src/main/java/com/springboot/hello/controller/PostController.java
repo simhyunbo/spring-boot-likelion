881898881d05d086f967da66fe702976aaefdc25
simhyunbo <hbo9347@naver.com>
2022-10-27 14:17:11 +0900

    Post RequestBody

--- hello/src/main/java/com/springboot/hello/controller/PostController.java
+++ hello/src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -10,5 +11,21 @@ public class PostController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample(){
         return "Hello Post";
+    }
+
+    //@RequestBody의 사용
+    //Post에서는 @RequestBody를 주로 사용 합니다.
+    //json(등)형식으로 넘어온 데이터를 사용할 수 있게 해줍니다.
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String , Object> postData){
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(map->sb.append(map.getKey()
+        +":"+map.getValue()+"\n"));
+        return sb.toString();
+    }
+    @PostMapping("/member2")
+    public String postMember2(@RequestBody MemberDto memberDto){
+
+        return memberDto.toString();
     }
 }
