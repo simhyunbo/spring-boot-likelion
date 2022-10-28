59d3e6afe915290402911736f6f3bab4fe65bd91
simhyunbo <hbo9347@naver.com>
2022-10-28 14:33:58 +0900

    User / UserController에 add생성

--- hello/src/main/java/com/springboot/hello/controller/UserController.java
+++ hello/src/main/java/com/springboot/hello/controller/UserController.java
@@ -1,12 +1,24 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.hello.dao.UserDao;
+import com.springboot.hello.domain.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+    UserDao userDao;
+    @PostMapping("/add")
+    public String add(@RequestBody User user) {
+        {
+            userDao.add(user);
+            return "add성공 : " + user.getId();
+        }
 
-
-
+//    @DeleteMapping
+//    public String deleteUser(){
+//        userDao.deleteAll();
+//        return "delete";
+//    }
+    }
 }
--- hello/src/main/java/com/springboot/hello/domain/User.java
+++ hello/src/main/java/com/springboot/hello/domain/User.java
@@ -1,25 +1,14 @@
 package com.springboot.hello.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@AllArgsConstructor
+@Getter
 public class User {
+
     private String id;
     private String name;
     private String password;
 
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
