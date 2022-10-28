2ac4d52c6bbe590298437f11300c6b3cbbc1698c
simhyunbo <hbo9347@naver.com>
2022-10-28 11:39:18 +0900

    log info 출력

--- hello/src/main/java/com/springboot/hello/controller/GetController.java
+++ hello/src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,24 +1,32 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+
+
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     //컨트롤러에 어떤 메소드를 할당할지 정해준다
     //@RestController : dispatcherServlet이 매핑해준 Controller 등록
     //@RequestMapping("path") : controller가 할당 된 후 어떤 method를 실행할지 연결해주는 어노테이션
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
+
     public String hello(){
+        //@Slf4j를 class위에 추가후 log.info()로 남깁니다.
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName(){
+        log.info("name으로 요청이 들어왔습니다.");
         return "name";
     }
 
