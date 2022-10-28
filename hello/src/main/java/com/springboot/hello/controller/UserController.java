722c55248cb8470dc45661161af583a4bb048e9f
simhyunbo <hbo9347@naver.com>
2022-10-28 15:45:09 +0900

    id로 Delete 기능 추가

--- hello/src/main/java/com/springboot/hello/controller/UserController.java
+++ hello/src/main/java/com/springboot/hello/controller/UserController.java
@@ -8,17 +8,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/user")
 public class UserController {
     UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @PostMapping("/add")
     public String add(@RequestBody User user) {
-        {
+
             userDao.add(user);
             return "add성공 : " + user.getId();
-        }
 
-//    @DeleteMapping
-//    public String deleteUser(){
-//        userDao.deleteAll();
-//        return "delete";
-//    }
     }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String delete(@PathVariable String id) {
+        userDao.delete(id);
+        return "delete성공";
+    }
+
+    @DeleteMapping(value = "/deleteAll")
+    public String deleteAll(){
+        userDao.deleteAll();
+        return "deleteAll 성공";
+    }
+
 }
+
+
+
--- hello/src/main/java/com/springboot/hello/dao/UserDao.java
+++ hello/src/main/java/com/springboot/hello/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.springboot.hello.dao;
 
+import com.springboot.hello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,17 @@ public class UserDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public int deleteAll(){
-        return this.jdbcTemplate.update("delete from users");
+
+    public void add(User user){
+
+        this.jdbcTemplate.update("insert into users(id,name,password) " +
+                "values(?,?,?)",user.getId(),user.getName(),user.getPassword());
+    }
+
+    public void delete(String id){
+        this.jdbcTemplate.update("delete from users where id = ?",id);
+    }
+    public void deleteAll(){
+        this.jdbcTemplate.update("delete from users");
     }
 }
