33c38193fa2061978b7dfbff0eb19f4f3a86ebf6
simhyunbo <hbo9347@naver.com>
2022-11-07 00:18:58 +0900

    HospitalApi 1.Data Insert 2. .findById()

--- hello/src/main/java/com/springboot/hello/controller/HospitalController.java
+++ hello/src/main/java/com/springboot/hello/controller/HospitalController.java
@@ -23,7 +23,7 @@ public class HospitalController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Hospital> get(@PathVariable int id) {
+    public ResponseEntity<Hospital> get(@PathVariable Integer id) {
         Hospital hospital = hospitalDao.findById(id);
         Optional<Hospital> opt = Optional.of(hospital);
 
